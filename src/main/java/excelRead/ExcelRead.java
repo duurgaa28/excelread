7b13ce99ccd0571b150e88ef5048f9e292c71dae
duurgaa28 <durgavijayakumar28@gmail.com>
2023-11-18 10:20:36 +0530

    formatted

--- src/main/java/excelRead/ExcelRead.java
+++ src/main/java/excelRead/ExcelRead.java
@@ -12,39 +12,39 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelRead {
 	XSSFSheet sheet;
-ExcelRead () throws IOException{
-	FileInputStream inputfile =new FileInputStream("\\C:\\Users\\lenovo\\Downloads\\Book1.xlsx");
-	XSSFWorkbook workbook=new XSSFWorkbook(inputfile);
-	sheet=workbook.getSheet("Sheet1");
-} 
-public String readExcelData(int i,int j) {
-	XSSFRow row=sheet.getRow(i);
-	Cell cell=row.getCell(j);
-	CellType type=cell.getCellType();
-	
-		switch(type){
+
+	ExcelRead() throws IOException {
+		FileInputStream inputfile = new FileInputStream("\\C:\\Users\\lenovo\\Downloads\\Book1.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(inputfile);
+		sheet = workbook.getSheet("Sheet1");
+	}
+
+	public String readExcelData(int i, int j) {
+		XSSFRow row = sheet.getRow(i);
+		Cell cell = row.getCell(j);
+		CellType type = cell.getCellType();
+
+		switch (type) {
 		case NUMERIC:
 			return String.valueOf(cell.getNumericCellValue());
 		case STRING:
-		return	cell.getStringCellValue();
+			return cell.getStringCellValue();
 		case BOOLEAN:
 			return String.valueOf(cell.getBooleanCellValue());
 		}
-		
-	
-		
-	return "  ";
-	}
-	
-public static void main (String args[]) throws IOException {
-	ExcelRead ex=new ExcelRead();
-	System.out.println("reading data from excel");
-	for(int i=0;i<6;i++) {
-		for(int j=0;j<3;j++) {
-			System.out.print(ex.readExcelData(i,j)+" ");
-		}
-		System.out.println();
+
+		return "  ";
 	}
 
-}
+	public static void main(String args[]) throws IOException {
+		ExcelRead ex = new ExcelRead();
+		System.out.println("reading data from excel");
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(ex.readExcelData(i, j) + " ");
+			}
+			System.out.println();
+		}
+
+	}
 }
